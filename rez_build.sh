486aeccd221bb6ad2b697d20ad46212851d6be24
Marcelo Sercheli <marcelo.sercheli@hernehill.com>
2024-07-23 10:48:23 -0400

    Clean

--- rez_build.sh
+++ rez_build.sh
@@ -10,12 +10,12 @@ echo "----------------------------------------------------------------------"
 make -f ${root_dir}/Makefile -j1 tbb_root=${root_dir} tbb_build_dir=${root_dir}/_rez_build
 
 echo "----------------------------------------------------------------------"
-echo "Copying the files to REZ location"
+echo "Copying files to REZ location"
 echo "----------------------------------------------------------------------"
 mkdir ${REZ_BUILD_INSTALL_PATH}/lib
 mkdir ${REZ_BUILD_INSTALL_PATH}/include
 
 cp -a ${root_dir}/_rez_build/*_release/libtbb*.* ${REZ_BUILD_INSTALL_PATH}/lib
-cp -a ${root_dir}/_rez_build/*_debug/libtbb*.* ${REZ_BUILD_INSTALL_PATH}/lib
+# cp -a ${root_dir}/_rez_build/*_debug/libtbb*.* ${REZ_BUILD_INSTALL_PATH}/lib
 cp -a ${root_dir}/include/serial ${REZ_BUILD_INSTALL_PATH}/include/serial
 cp -a ${root_dir}/include/tbb ${REZ_BUILD_INSTALL_PATH}/include/tbb
